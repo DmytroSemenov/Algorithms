641d920b2938b82b4bc77bcc0445ae84873cd677
Semenov <dmytro.semenov@sigmaukraine.com>
2019-08-30 17:47:20 +0300

    test error has been fixed

--- tests/test.js
+++ tests/test.js
@@ -6,17 +6,20 @@ const expectedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 describe('Testing insertion sort algorithm', () => {
     it('insertion sort', () => {
-        const myArray = new SmartArray(unsortedArray);
+        const myArray = new SmartArray();
+        myArray.setArray(unsortedArray);
         const sortedInsertionArray = myArray.insertionSort();
         assert.equal(expectedArray.toString(), sortedInsertionArray.toString());
     });
     it('quick sort', () => {
-        const myArray1 = new SmartArray(unsortedArray);
+        const myArray1 = new SmartArray();
+        myArray1.setArray(unsortedArray);
         const sortedQuickArray = myArray1.quickSort();
         assert.equal(expectedArray.toString(), sortedQuickArray.toString());
     });
     it('merge sort', () => {
-        const myArray2 = new SmartArray(unsortedArray);
+        const myArray2 = new SmartArray();
+        myArray2.setArray(unsortedArray);
         const sortedMergeArray = myArray2.mergeSort();
         assert.equal(expectedArray.toString(), sortedMergeArray.toString());
     });
@@ -25,7 +28,8 @@ describe('Testing insertion sort algorithm', () => {
 describe('Testing swap elements method', function() {
     const unsortedArray = [9, 1, 2, 3, 4, 5, 6, 7, 8, 0];
     const expectedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const testArray = new SmartArray(unsortedArray);
+    const testArray = new SmartArray();
+    testArray.setArray(unsortedArray);
 
     it('swap elements 0, 9', () => {
         testArray._swapElements(0, 9);
